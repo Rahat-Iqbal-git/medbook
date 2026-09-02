f9363fb0ab659b27c1285d4c34466f40a9cdc6b6
Rahat Iqbal <81703704+Rahat-Iqbal-git@users.noreply.github.com>
2026-09-02 16:12:47 +0600

    feat: implement failure handling classes and add fpdart dependency

---
+++ lib/core/failures/failure.dart
@@ -0,0 +1,26 @@
+sealed class Failure {
+  const Failure({required this.message});
+
+  final String message;
+}
+
+final class ApiFailure extends Failure {
+  const ApiFailure({String? message, this.statusCode})
+    : super(message: message ?? 'Network Failure');
+
+  final int? statusCode;
+}
+
+final class ConnectionFailure extends Failure {
+  const ConnectionFailure({String? message})
+    : super(message: message ?? 'Connection Failure');
+}
+
+final class ParsingFailure extends Failure {
+  const ParsingFailure({String? message})
+    : super(message: message ?? 'Parsing Failure');
+}
+
+final class ValidationFailure extends Failure {
+  const ValidationFailure({required super.message});
+}
