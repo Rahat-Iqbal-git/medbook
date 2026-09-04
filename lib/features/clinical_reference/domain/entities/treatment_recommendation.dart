e5248539143df4d5c2edc8ac209b543c1f9933e4
Rahat Iqbal <81703704+Rahat-Iqbal-git@users.noreply.github.com>
2026-09-05 00:42:51 +0600

    feat: remove title and notes fields from TreatmentRecommendation entity

--- lib/features/clinical_reference/domain/entities/treatment_recommendation.dart
+++ lib/features/clinical_reference/domain/entities/treatment_recommendation.dart
@@ -4,21 +4,17 @@ class TreatmentRecommendation {
     required this.id,
     required this.diseaseId,
     required this.medicineId,
-    required this.title,
     required this.type,
     required this.dose,
     required this.frequency,
     required this.duration,
-    required this.notes,
   });
 
   final int id;
   final int diseaseId;
   final int medicineId;
-  final String title;
   final String type;
   final String dose;
   final String frequency;
   final String duration;
-  final String notes;
 }
